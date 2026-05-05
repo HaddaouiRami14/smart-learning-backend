adcbd31c3d2254694f07fab2e6ff8330e9d3557b
HaddaouiRami14 <mohamedramihaddaoui@gmail.com>
2026-05-05 13:19:02 +0100

    Ajouter un endpoint pour calculer le nombre des apprenants et l'afficher dans la page d'accueil

--- src/main/java/com/example/SmartLearning/Repository/UserRepository.java
+++ src/main/java/com/example/SmartLearning/Repository/UserRepository.java
@@ -13,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
     boolean existsByEmail(String email);
     Optional<User> findByRole(Role role);
     Optional<User> findByGoogleId(String googleId);
+    long countByRole(Role role);
     
 }
--- src/main/java/com/example/SmartLearning/controller/CourseController.java
+++ src/main/java/com/example/SmartLearning/controller/CourseController.java
@@ -1,7 +1,5 @@
 package com.example.SmartLearning.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SmartLearning.DTO.CourseDTO;
+import com.example.SmartLearning.Enum.Role;
+import com.example.SmartLearning.Repository.UserRepository;
 import com.example.SmartLearning.service.CourseService;
 
 @RestController
@@ -18,10 +18,19 @@ public class CourseController {
 @Autowired
 private CourseService courseService;
 
+@Autowired
+private UserRepository userRepository;
+
+
  @GetMapping
  public ResponseEntity<List<CourseDTO>> getAllCourses() {
  List<CourseDTO> courses = courseService.getAllCourses();
  return ResponseEntity.ok(courses);
  }
 
+@GetMapping("/count-learners")
+public long countLearners() {
+    return userRepository.countByRole(Role.APPRENANT);
+}
+  
 }
